916c6a99cb7456a1c30a11988c3845a89cf23e39
deepak.janmanchi <deepak.janmanchi@servicenow.com>
2023-05-05 15:56:47 +0530

    sonar Testing 26

--- src/main.js
+++ src/main.js
@@ -55,84 +55,67 @@ const axios = require('axios');
         return;
     }
 
-    let result;
     const endpointv1 = `${instanceUrl}/api/sn_devops/v1/devops/tool/softwarequality?toolId=${toolId}`;
     const endpointv2 = `${instanceUrl}/api/sn_devops/v2/devops/tool/softwarequality?toolId=${toolId}`;
     let endpoint;
     let httpHeaders;
-    
-    // function _prepareSignature(secret, payload, shaAlgorithm) {
-    //     var base64EncodedSignature = new CertificateEncryption.generateMac(gs.base64Encode(secret), shaAlgorithm, payload);
-    //     return _base64toHex(base64EncodedSignature);
-    // }
 
-    // function _base64toHex(base64Signatrue) {
-    //     var base64Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-    //     var base64Lookup = {};
-    //     for (var i = 0; i < base64Alphabet.length; i++)
-    //         base64Lookup[base64Alphabet.charAt(i)] = i;
-    //     base64Lookup['='] = 0;
-
-    //     var hexAlphabet = "0123456789abcdef";
-
-    //     var pad = 0;
-    //     if (base64Signatrue.charAt(base64Signatrue.length - 1) === '=') {
-    //         pad++;
-
-    //         if (base64Signatrue.charAt(base64Signatrue.length - 2) === '=')
-    //             pad++;
-    //     }
-
-    //     var hex = [];
-
-    //     var idx = 0;
-    //     while (idx < base64Signatrue.length) {
-    //         var bits = 0;
-    //         bits |= base64Lookup[base64Signatrue.charAt(idx++)] << 18;
-    //         bits |= base64Lookup[base64Signatrue.charAt(idx++)] << 12;
-    //         bits |= base64Lookup[base64Signatrue.charAt(idx++)] << 6;
-    //         bits |= base64Lookup[base64Signatrue.charAt(idx++)];
-
-    //         hex.push(hexAlphabet[(bits >> 20) & 0xF]);
-    //         hex.push(hexAlphabet[(bits >> 16) & 0xF]);
-
-    //         if (idx != base64Signatrue.length || pad < 2) {
-    //             hex.push(hexAlphabet[(bits >> 12) & 0xF]);
-    //             hex.push(hexAlphabet[(bits >> 8) & 0xF]);
-    //         }
-
-    //         if (idx != base64Signatrue.length || pad < 1) {
-    //             hex.push(hexAlphabet[(bits >> 4) & 0xF]);
-    //             hex.push(hexAlphabet[bits & 0xF]);
-    //         }
-    //     }
-
-    //     return hex.join('');
-    // }
-
-    try {
-        if (secretToken) {
+    // try {
+    //     if (secretToken) {
   
-            // const shaAlgorithm = 'HmacSHA256';
-            // console.log("payload: "+JSON.stringify(payload));
-            // const calculateSignature = _prepareSignature(secretToken, JSON.stringify(payload), shaAlgorithm);
-            // console.log("signature: "+calculateSignature);
-            // calculateSignature = 'sha256=' + calculateSignature;
+    //         const defaultHeadersv2 = {
+    //             'Content-Type': 'application/json',
+    //             'Accept': 'application/json',
+    //             'Authorization': `${secretToken}`
+    //         };
+    //         httpHeaders = {
+    //             headers: defaultHeadersv2
+    //         };
+    //         endpoint = endpointv2;
+    //         console.log("Secret Token if code ");
+    //     }
+    //     else if (username && password) {
+    //         const token = `${username}:${password}`;
+    //         const encodedToken = Buffer.from(token).toString('base64');
+    //         const defaultHeadersv1 = {
+    //             'Content-Type': 'application/json',
+    //             'Accept': 'application/json',
+    //             'Authorization': 'Basic ' + `${encodedToken}`
+    //         };
+    //         httpHeaders = {
+    //             headers: defaultHeadersv1
+    //         };
+    //         endpoint = endpointv1;
+    //         console.log("username and password if code ");
+    //     } else {
+    //         throw "Credentials are empty";
+    //     }
+    //     console.log("endpoint: "+endpoint);
+    //     snowResponse = await axios.post(endpoint, JSON.stringify(payload), httpHeaders);
+    // } catch (e) {
+    //     if (e.message.includes('ECONNREFUSED') || e.message.includes('ENOTFOUND') || e.message.includes('405')) {
+    //         core.setFailed('ServiceNow Instance URL is NOT valid. Please correct the URL and try again.');
+    //     } else if (e.message.includes('401')) {
+    //         core.setFailed('Invalid Credentials. Please correct the credentials and try again.');
+    //     } else {
+    //         core.setFailed(`ServiceNow Software Quality Results are NOT created. Please check ServiceNow logs for more details.`);
+    //     }
+    // }
+    try {
+        if (!secretToken && !username && !password) {
+            core.setFailed('Either secret token or integration username, password is needed for integration user authentication');
+            return;
+        } else if (secretToken) {
             const defaultHeadersv2 = {
                 'Content-Type': 'application/json',
                 'Accept': 'application/json',
-                'Authorization': `${secretToken}`
-                //'x-hub-signature-256': `${calculateSignature}`
-                //  'token': `${ni.nolog.token}`
-                //'Authorization': 'x-hub-signature-256 '+`${secretToken}`
+                'Authorization': `${securityToken}`
             };
             httpHeaders = {
                 headers: defaultHeadersv2
             };
             endpoint = endpointv2;
-            console.log("Secret Token if code ");
-        }
-        else if (username && password) {
+        } else if (username && password) {
             const token = `${username}:${password}`;
             const encodedToken = Buffer.from(token).toString('base64');
             const defaultHeadersv1 = {
@@ -144,11 +127,10 @@ const axios = require('axios');
                 headers: defaultHeadersv1
             };
             endpoint = endpointv1;
-            console.log("username and password if code ");
         } else {
-            throw "Credentials are empty";
+            core.setFailed('For Basic Auth, Username and Password is mandatory for integration user authentication');
+            return;
         }
-        console.log("endpoint: "+endpoint);
         snowResponse = await axios.post(endpoint, JSON.stringify(payload), httpHeaders);
     } catch (e) {
         if (e.message.includes('ECONNREFUSED') || e.message.includes('ENOTFOUND') || e.message.includes('405')) {
@@ -156,10 +138,9 @@ const axios = require('axios');
         } else if (e.message.includes('401')) {
             core.setFailed('Invalid Credentials. Please correct the credentials and try again.');
         } else {
-            core.setFailed(`ServiceNow Software Quality Results are NOT created. Please check ServiceNow logs for more details.`);
+            core.setFailed('ServiceNow Software Quality Results are NOT created. Please check ServiceNow logs for more details.');
         }
-    }
-    
+    }  
 })();
 
 
